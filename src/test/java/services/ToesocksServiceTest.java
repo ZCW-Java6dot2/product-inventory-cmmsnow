08d2942f2c501bd1a4b79ba2152b1993b6abd914
christine <christine@zipcodes-MacBook-Pro-10.local>
2020-11-07 21:51:17 -0500

    completed and passed all tests

--- src/test/java/services/ToesocksServiceTest.java
+++ src/test/java/services/ToesocksServiceTest.java
@@ -41,42 +41,52 @@ public class ToesocksServiceTest {
     }
 
     public void findToesocksTest(){
-        //(1)expected
-        int expectedId = 4;
-
-        //(2)
+        //
         ToesocksService toesocksService = new ToesocksService();
-        Toesocks testToesocks = toesocksService.findToesocks(int id);
+        Toesocks testToesocks = toesocksService.create("Floofy", "Booboo's", "Couch", 7, 10, 9.00f);
 
-        //(3)actual
-        int actualId = testToesocks.getId();
 
-        //(4)assertEquals
-        Assertions.assertEquals(expectedId, actualId);
+        //assertEquals
+        Toesocks expectedToesocks = toesocksService.findToesocks(0);
+        Assertions.assertEquals(expectedToesocks, testToesocks);
     }
 
     public void findAllTest(){
-        //(1)expected
+        ToesocksService toesocksService = new ToesocksService();
+        Toesocks[] toeArray = new Toesocks[2];
+
+        //(1)
+        Toesocks testSocks0 = toesocksService.create("Floofy", "Booboo's", "Couch", 7, 10, 9.00f);
+        Toesocks expectedToesocks0 = toesocksService.findToesocks(0);
+        toeArray[0] = testSocks0;
+        Assertions.assertEquals(expectedToesocks0, testSocks0);
 
         //(2)
-        ToesocksService toesocksService = new ToesocksService();
-        Toesocks testToesocks = toesocksService.findAll();
+        Toesocks testSocks1 = toesocksService.create("Snug", "Booboo's", "Couch", 7, 10, 9.00f);
+        Toesocks expectedToesocks1 = toesocksService.findToesocks(1);
+        toeArray[1] = testSocks0;
+        Assertions.assertEquals(expectedToesocks1, testSocks1);
 
-        //(3)actual
-        int actualId;
+        //(3)expected
+        Toesocks[] toesocksExpected = toesocksService.findAll();
 
         //(4)assertEquals
+        Assertions.assertEquals(toeArray[0], toesocksExpected[0]);
+        Assertions.assertEquals(toeArray[1], toesocksExpected[1]);
     }
 
     public void deleteTest(){
+        ToesocksService toesocksService = new ToesocksService();
+
         //(1) expected
+        Toesocks testToesocks0 = toesocksService.create("Floofy", "Booboo's", "Couch", 7, 10, 9.00f);
+        Toesocks testToesocks1 = toesocksService.create("Snug", "Booboo's", "Couch", 7, 10, 9.00f);
 
         //(2)
-        ToesocksService toesocksService = new ToesocksService();
-        //Toesocks testToesocks = toesocksService.delete(int id);
+        boolean toesocksExpected = toesocksService.delete(1);
 
-        //(3) actual
-
-        //(4) assertEquals(expected, actual)
+        //(3) assertEquals
+        Assertions.assertEquals(toesocksExpected, true);
+        Assertions.assertEquals(toesocksService.findToesocks(1), null);
     }
 }
